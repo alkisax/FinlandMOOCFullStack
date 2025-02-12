56d132891eb405f550aead090943db1fbfd3de13
Alkis <alkisax@gmail.com>
2025-02-12 23:04:35 +0200

    2.11 ok

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,34 @@
 //
-
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setnewNumber] = useState('')
   const [filter, setFilter] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then (response => {
+        console.log(response.data)
+        setPersons(response.data)
+        // setPersons(response.name)
+        // setnewNumber(response.number)
+      })
+  }
+  useEffect(hook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
