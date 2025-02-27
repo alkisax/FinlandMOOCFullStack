2ed6efa7436b6b1179a7422e1cc958774169d3b1
Alkis <alkisax@gmail.com>
2025-02-27 12:02:33 +0200

    3.21 ok

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,7 +1,23 @@
 import axios from 'axios'
 // const baseUrl = 'http://localhost:3001/persons'
+// const baseUrl = import.meta.env.VITE_API_URL
 // const baseUrl = import.meta.env.VITE_API_URL ||   'https://finlandmoocfullstackpart3.onrender.com/api/persons' // this because they where uploaded as same service on render
-const baseUrl = 'https://finlandmoocfullstackpart3.onrender.com/api/persons' // this because they where uploaded as same service on render
+// const baseUrl = 'https://finlandmoocfullstackpart3.onrender.com/api/persons' 
+
+const LOCAL_URL = 'http://localhost:3001/api/persons';
+const REMOTE_URL = 'https://finlandmoocfullstackpart3.onrender.com/api/persons';
+let baseUrl = REMOTE_URL; // Default to remote
+axios
+  .get(LOCAL_URL)
+  .then((response) => {
+    if (response.status === 200) {
+      console.log("Using local server");
+      baseUrl = LOCAL_URL;
+    }
+  })
+  .catch(() => {
+    console.log("Local server not found, using remote API.");
+  });
 
 
 const getAll = () => {
