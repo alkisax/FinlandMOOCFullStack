98022aee3a3a94069e8cb6ac856c53dfcebcba1e
Alkis <alkisax@gmail.com>
2025-03-02 21:45:24 +0200

    4.6 ok

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -32,8 +32,49 @@ const favoriteBlog = (blogs) => {
   return topBlog
 }
 
+const mostBlogs = (blogs) => {
+  // create a list of object with authors/blogs
+  const auth_blog = []
+  blogs.forEach(blog => {
+    let found = false
+    auth_blog.forEach(entry => {
+      if (entry.author === blog.author){
+        entry.blogs += 1
+        found = true
+      }
+    })
+    if (!found) {
+      auth_blog.push({
+        author: blog.author,
+        blogs: 1
+      })
+    }
+  })
+
+  //create a list with the top author (or more if same no)
+  let max = -Infinity
+  const topAuth = []
+    // part 1 find higher no of blogs
+    auth_blog.forEach(auth => {
+      if (auth.blogs >= max){
+        max = auth.blogs
+      }
+    })
+    //part 2 add blogs with max likes to arr
+    auth_blog.forEach(auth => {
+      if (auth.blogs === max) {
+        topAuth.push({
+          author: auth.author,
+          blogs: auth.blogs
+        })
+      }
+    })
+    return topAuth[0]
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
