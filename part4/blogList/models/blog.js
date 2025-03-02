bdb6b6d83200dd91a1388b2818d776c0c5bf67ca
Alkis <alkisax@gmail.com>
2025-03-02 12:56:49 +0200

    start of 4.1

---
+++ part4/blogList/models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString() // Convert _id (ObjectId) to a string and store it in 'id'
+    delete returnedObject._id // Remove the original _id field
+    delete returnedObject.__v // Remove the __v field (used for versioning by Mongoose)
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
