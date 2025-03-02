31070afc04a7f8b95b31fc5c9bd039faae0396ab
Alkis <alkisax@gmail.com>
2025-03-02 20:43:47 +0200

    4.5 ok

--- part4/blogList/tests/dummy.test.js
+++ part4/blogList/tests/dummy.test.js
@@ -2,6 +2,23 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const blogs = [
+  {
+    "title": "My first blog",
+    "author": "John Doe",
+    "url": "https://example.com",
+    "likes": 5,
+    "id": "67c43dfbd6f18f0154eb45be"
+  },
+  {
+    "title": "My second blog",
+    "author": "Johan Doe",
+    "url": "https://example2.com",
+    "likes": 6,
+    "id": "67c43e35d6f18f0154eb45c1"
+  }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -10,22 +27,6 @@ test('dummy returns one', () => {
 })
 
 describe ('totalLikes', () => {
-  const blogs = [
-    {
-      "title": "My first blog",
-      "author": "John Doe",
-      "url": "https://example.com",
-      "likes": 5,
-      "id": "67c43dfbd6f18f0154eb45be"
-    },
-    {
-      "title": "My second blog",
-      "author": "Johan Doe",
-      "url": "https://example2.com",
-      "likes": 6,
-      "id": "67c43e35d6f18f0154eb45c1"
-    }
-  ]
   test('total likes is 11', ()  => {
     const result = listHelper.totalLikes(blogs)
     assert.strictEqual(result, 11)
@@ -39,5 +40,32 @@ describe ('totalLikes', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
   })
+})
+
+describe ('maxLikes', () => {
+  test('max likes is 6', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, [{
+      "title": "My second blog",
+      "author": "Johan Doe",
+      "likes": 6
+    }])
+  })
+
+  test('returns empty array for an empty list', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.deepStrictEqual(result, [])
+  })
+
+  test('when list has only one blog, returns that blog', () => {
+    const singleBlogList = [blogs[0]]
+    const result = listHelper.favoriteBlog(singleBlogList)
+    assert.deepStrictEqual(result, [{
+      "title": "My first blog",
+      "author": "John Doe",
+      "likes": 5
+    }])
+  })
+
 
 })
