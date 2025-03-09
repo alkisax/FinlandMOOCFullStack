d4544b061a1c30ba413d298780721ad1da3404a4
Alkis <alkisax@gmail.com>
2025-03-09 17:28:56 +0200

    4.20 ok

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -8,19 +8,20 @@ blogRouter.get('/', async (req, res) => {
   res.json(blogs)
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.startsWith('Bearer ')) {
+//     return authorization.replace('Bearer ', '')
+//   }
+//   return null
+// }
 
 blogRouter.post('/', async (req, res) => {
 
   const body = req.body
 
-  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+  // const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
   if (!decodedToken.id) {
     return res.status(401).json({ error: 'token invalid' })
   }
