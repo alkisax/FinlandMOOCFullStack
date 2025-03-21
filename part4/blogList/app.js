efdef6e7a6c5918ded25b36c935994ca74919322
Alkis <alkisax@gmail.com>
2025-03-21 23:32:04 +0200

    working on cypress added tester server to back

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -12,16 +12,21 @@ const blogRouter  = require('./controllers/blogs')
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
-// app.use(middleware.tokenExtractor)
-// app.use(middleware.userExtractor)
 app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor, blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  console.log("testing server")
+  console.log('Current NODE_ENV:', process.env.NODE_ENV);
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 mongoose.set('strictQuery', false)
-// logger.info('Connecting to ', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
