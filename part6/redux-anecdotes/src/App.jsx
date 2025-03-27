e4dd95af98a920c05487badb357704afe8705d6f
Alkis <alkisax@gmail.com>
2025-03-27 15:04:54 +0200

    working on 6.9

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import AnecdoteFilter from './components/AnecdoteFilter'
  
 const App = () => {
 
 
   return (
     <div>
+      <AnecdoteFilter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
