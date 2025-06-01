724b6d9d48b203e0b98d24c0e9c844b17810366b
Alkis <alkisax@gmail.com>
2025-06-01 21:41:02 +0300

    9.3 ok

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,30 +1,41 @@
+try {
+  const bmiArgs: string[] = process.argv.slice(2)
 
-const bmiArgs: string[] = process.argv.slice(2)
-const height: number = Number(args[0])
-const mass: number = Number(args[1])
+  if (bmiArgs.length < 2) throw new Error('Not enough arguments');
+  if (bmiArgs.length > 2) throw new Error('Too many arguments');
 
+  const height: number = Number(bmiArgs[0])
+  const mass: number = Number(bmiArgs[1])
 
-const calculateBmi  = (height: number, mass: number): string => {
-  const heightInMeters: number = height / 100;
-  const bmi: number =  mass / (heightInMeters**2)
+  const calculateBmi  = (height: number, mass: number): string => {
+    const heightInMeters: number = height / 100;
+    const bmi: number =  mass / (heightInMeters**2)
 
-  if (bmi < 16.0) {
-    return 'Underweight (Severe thinness)';
-  } else if (bmi < 17.0) {
-    return 'Underweight (Moderate thinness)';
-  } else if (bmi < 18.5) {
-    return 'Underweight (Mild thinness)';
-  } else if (bmi < 25.0) {
-    return 'Normal range';
-  } else if (bmi < 30.0) {
-    return 'Overweight (Pre-obese)';
-  } else if (bmi < 35.0) {
-    return 'Obese (Class I)';
-  } else if (bmi < 40.0) {
-    return 'Obese (Class II)';
+    if (bmi < 16.0) {
+      return 'Underweight (Severe thinness)';
+    } else if (bmi < 17.0) {
+      return 'Underweight (Moderate thinness)';
+    } else if (bmi < 18.5) {
+      return 'Underweight (Mild thinness)';
+    } else if (bmi < 25.0) {
+      return 'Normal range';
+    } else if (bmi < 30.0) {
+      return 'Overweight';
+    } else if (bmi < 35.0) {
+      return 'Obese (Class I)';
+    } else if (bmi < 40.0) {
+      return 'Obese (Class II)';
+    } else {
+      return 'Obese (Class III)';
+    }
+  }
+
+  console.log(calculateBmi(height, mass))
+
+} catch (error: unknown){
+  if (error instanceof Error) {
+    console.error('Error:', error.message);
   } else {
-    return 'Obese (Class III)';
+    console.error('Unknown error occurred');
   }
 }
-
-console.log(calculateBmi(height, mass))
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -10,56 +10,70 @@ interface exercise{
   average: number
 }
 
-const args: string[] = process.argv.slice(2)
-const target: number = Number(args[0]);
-const inputNumbers: number[] = args.slice(1).map(Number);
-if (inputNumbers.some(isNaN)) {
-  throw new Error('One or more arguments are not valid numbers');
+try {
+  const args: string[] = process.argv.slice(2)
+
+  if (args.length < 2) throw new Error('Not enough arguments');
+
+  const target: number = Number(args[0]);
+  const inputNumbers: number[] = args.slice(1).map(Number);
+  if (inputNumbers.some(isNaN)) {
+    throw new Error('One or more arguments are not valid numbers');
+  }
+
+  const trainingDaysArr: number[] = inputNumbers.filter((el => el !== 0))
+
+  const sum: number = inputNumbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0)
+  const average: number = sum / inputNumbers.length
+
+  let rating: number;
+  if (average >= target) {
+    rating = 3
+  } else if (average < target && average >= target * 0.75) {
+    rating = 2
+  } else {
+    rating = 1
+  }
+
+  let ratingDescription: string;
+  switch (rating) {
+    case 3:
+      ratingDescription = "Great job! You met your exercise goal.";
+      break;
+    case 2:
+      ratingDescription = "Not too bad but could be better.";
+      break;
+    case 1:
+      ratingDescription = "You need to exercise more.";
+      break;
+    default:
+      ratingDescription = "Rating not found.";
+  }
+
+
+  const result: exercise = {
+    periodLength: inputNumbers.length,
+    trainingDays: trainingDaysArr.length,
+    success: average >= target,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average
+  } 
+
+  console.log(result)
+
+} catch (error: unknown){
+  if (error instanceof Error) {
+    console.error('Error:', error.message);
+  } else {
+    console.error('Unknown error occurred');
+  }
 }
 
 
-const trainingDaysArr: number[] = inputNumbers.filter((el => el !== 0))
 
-const sum: number = inputNumbers.reduce((accumulator, currentValue) => {
-  return accumulator + currentValue;
-}, 0)
-const average: number = sum / inputNumbers.length
-
-let rating: number;
-if (average >= target) {
-  rating = 3
-} else if (average < target && average >= target * 0.75) {
-  rating = 2
-} else {
-  rating = 1
-}
-
-let ratingDescription: string;
-switch (rating) {
-  case 3:
-    ratingDescription = "Great job! You met your exercise goal.";
-    break;
-  case 2:
-    ratingDescription = "Not too bad but could be better.";
-    break;
-  case 1:
-    ratingDescription = "You need to exercise more.";
-    break;
-  default:
-    ratingDescription = "Rating not found.";
-}
-
-
-const result: exercise = {
-  periodLength: inputNumbers.length,
-  trainingDays: trainingDaysArr.length,
-  success: average >= target,
-  rating: rating,
-  ratingDescription: ratingDescription,
-  target: target,
-  average: average
-} 
-
-console.log(result)
 
 
