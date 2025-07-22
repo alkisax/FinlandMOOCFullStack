80fc111938fac9b7a1a35e79180ea90093a080a2
Alkis <alkisax@gmail.com>
2025-07-22 10:43:10 +0300

    9.5 ok

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,60 +1,59 @@
-interface ArgValues {
+export interface ArgValues {
   height: number;
   mass: number;
 }
 
-try {
-  // const bmiArgs: string[] = process.argv.slice(2)
-  const parseArguments = (args: string[]): ArgValues => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
-    
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-      return {
-        height: Number(args[2]),
-        mass: Number(args[3])
-      }
-    } else {
-      throw new Error('Provided values were not numbers!');
-    }
-  }
-
-  const calculateBmi  = (height: number, mass: number): string => {
-    const heightInMeters: number = height / 100;
-    const bmi: number =  mass / (heightInMeters**2)
-
-    if (bmi < 16.0) {
-      return 'Underweight (Severe thinness)';
-    } else if (bmi < 17.0) {
-      return 'Underweight (Moderate thinness)';
-    } else if (bmi < 18.5) {
-      return 'Underweight (Mild thinness)';
-    } else if (bmi < 25.0) {
-      return 'Normal range';
-    } else if (bmi < 30.0) {
-      return 'Overweight';
-    } else if (bmi < 35.0) {
-      return 'Obese (Class I)';
-    } else if (bmi < 40.0) {
-      return 'Obese (Class II)';
-    } else {
-      return 'Obese (Class III)';
-    }
-  }
-
-  const data: ArgValues = parseArguments(process.argv)
-  const height: number = data.height
-  const mass: number = data.mass
-  const responce = calculateBmi(height, mass)
-  console.log(responce);
+// const bmiArgs: string[] = process.argv.slice(2)
+export const parseArguments = (args: string[]): ArgValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
   
-
-} catch (error: unknown){
-  let errorMessage = 'Something bad happend'
-  if (error instanceof Error) {
-    errorMessage += 'Error: ' + error.message
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
-  console.log(errorMessage);
+}
+
+export const calculateBmi  = (height: number, mass: number): string => {
+  const heightInMeters: number = height / 100;
+  const bmi: number =  mass / (heightInMeters**2)
+
+  if (bmi < 16.0) {
+    return 'Underweight (Severe thinness)';
+  } else if (bmi < 17.0) {
+    return 'Underweight (Moderate thinness)';
+  } else if (bmi < 18.5) {
+    return 'Underweight (Mild thinness)';
+  } else if (bmi < 25.0) {
+    return 'Normal range';
+  } else if (bmi < 30.0) {
+    return 'Overweight';
+  } else if (bmi < 35.0) {
+    return 'Obese (Class I)';
+  } else if (bmi < 40.0) {
+    return 'Obese (Class II)';
+  } else {
+    return 'Obese (Class III)';
+  }
 }
 
 
+if (require.main === module) {
+  try {
+    const data: ArgValues = parseArguments(process.argv)
+    const height: number = data.height
+    const mass: number = data.mass
+    const response = calculateBmi(height, mass)
+    console.log(response);
+  } catch (error: unknown){
+    let errorMessage = 'Something bad happend'
+    if (error instanceof Error) {
+      errorMessage += 'Error: ' + error.message
+    }
+    console.log(errorMessage);
+  }
+}
--- part9/index.ts
+++ part9/index.ts
@@ -1,12 +1,39 @@
 import express from 'express';
 const app = express()
+import { calculateBmi } from './bmiCalculator'
 
 app.get('/hello', (_request, response) => {
   response.send('Hello Full Stack!')
 })
 
-const PORT = 3003
+app.get('/bmi', (request, response) => {
+  try {
+    const height = Number(request.query.height)
+    const weight = Number(request.query.weight)
 
+    if (isNaN(height) || isNaN(weight)) {
+      response.status(400).send({ error: 'malformatted parameters' })
+    }
+
+    const result: string = calculateBmi(height, weight)
+    const responseObj = {
+      weight: weight,
+      height: height,
+      bmi: result
+    }
+    response.status(200).send(responseObj)
+
+  } catch (error: unknown) {
+    let errorMessage = 'malformatted parameters'
+    if (error instanceof Error) {
+      errorMessage += 'error: ' + error.message
+    }
+    response.status(400).send(errorMessage);
+    return;
+  }
+})
+
+const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
