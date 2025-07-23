7b5d9be7f0516587b4262fb6ea7cd07e461f3dd2
Alkis <alkisax@gmail.com>
2025-07-23 17:51:38 +0300

    9.8 ok

---
+++ part9/patientor/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
