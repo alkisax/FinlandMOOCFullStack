7500f25a860f0bd6bcbbc1ab66c20a8989c8dbcf
Alkis <alkisax@gmail.com>
2025-07-26 17:33:30 +0300

    9.15 ok

--- part9/part9d/src/main.tsx
+++ part9/part9d/src/main.tsx
@@ -1,18 +1,77 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
+// import React from "react";
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+import type React from "react";
+
+const Header = ({ courseName }: { courseName: string}): React.ReactElement => {
+
+  return (
+    <>
+      <h1>{courseName}</h1>
+    </>
 
 
-import ReactDOM from 'react-dom/client'
-import App from './App';
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-)
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const Content  = ({courseParts}: {courseParts: CoursePart[]}): React.ReactElement => {
+
+  return (
+    <>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </>
+  )
+}
+
+const Total = ({ totalExercises }: {totalExercises: number}): React.ReactElement => {
+
+  return (
+    <>
+      <p>
+        Number of exercises {totalExercises}
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
+
+  return (
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
