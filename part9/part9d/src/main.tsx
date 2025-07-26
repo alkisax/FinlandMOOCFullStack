6109393e18889d1490e6144db60345cbf3bb1e80
Alkis <alkisax@gmail.com>
2025-07-26 16:09:20 +0300

    9.15 ok

---
+++ part9/part9d/src/main.tsx
@@ -0,0 +1,18 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import ReactDOM from 'react-dom/client'
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
+)
